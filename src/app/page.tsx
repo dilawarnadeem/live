523a4953eb2bc47b42d805900c229011b77d260d
mufaqar <mufaqar@gmail.com>
2024-03-10 15:57:36 +0500

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import { redirect } from 'next/navigation'
- 
 
-export default async function Profile() {
-  
+export default async function ReDirectTV() {  
     redirect('https://www.youtube.com/watch?v=ZgI0_Nmw_iU&ab_channel=PaighamTVLive')
- 
- 
-  // ...
+
 }
