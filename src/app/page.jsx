e1fb8abeefc34f3d41e75542d8b2d6309e0241db
mufaqar <mufaqar@gmail.com>
2024-03-13 18:54:41 +0500

    updated

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+const SomePage = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="w-full max-w-screen-xl">
+        <div className="w-full flex justify-center">
+          <iframe
+            className="w-full max-w-screen-2xl"
+            src="https://sscsott.com/pk/paighamtv/embed.html"
+            frameBorder="0"
+            scrolling="no"
+            width="100%"
+            height="760px"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SomePage;
