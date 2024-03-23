f299bc97a2312f711fad41cc405f87dfb6046670
mt09017 <45143455+mt09017@users.noreply.github.com>
2024-03-23 19:57:08 +0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export default async function ReDirectTV() {  
-    redirect('https://www.youtube.com/live/2WbgZiicUUU?si=mgG4Z_Rs8R0y8jd6')
+    redirect('https://www.youtube.com/channel/UC7hyoc-WXYfxHRZQAMWRyYQ/live')
 
 }
